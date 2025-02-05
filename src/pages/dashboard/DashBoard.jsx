d346f5547b63c2f1d4a4d4068a2604cc7132e29b
PunkHero98 <lqhuy1708@gmail.com>
2025-02-05 17:23:14 +0700

    updated chartjs

--- src/pages/dashboard/DashBoard.jsx
+++ src/pages/dashboard/DashBoard.jsx
@@ -1,5 +1,42 @@
+import { Bar } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+ChartJS.register(BarElement, CategoryScale, LinearScale, Tooltip, Legend);
+
 const DashBoard = () => {
-  return <p>Dashboard</p>;
+  const data = {
+    labels: ["Tháng 1", "Tháng 2", "Tháng 3", "Tháng 4", "Tháng 5"],
+    datasets: [
+      {
+        label: "Doanh thu (triệu VND)",
+        data: [120, 190, 300, 500, 200],
+        backgroundColor: "rgba(75, 192, 192, 0.6)", // Màu cột
+        borderColor: "rgba(75, 192, 192, 1)", // Viền cột
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: { position: "top" },
+      tooltip: { enabled: true },
+    },
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  };
+  return <Bar data={data} options={options} />;
 };
 
 export default DashBoard;
