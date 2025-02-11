00572c7bb84124ed1a1d1115727fb7b52bfdd04e
PunkHero98 <lqhuy1708@gmail.com>
2025-02-11 17:57:24 +0700

    updated

--- src/pages/manage/manageUser/ManageUser.jsx
+++ src/pages/manage/manageUser/ManageUser.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { Table, Space, Tag, Input, Button } from "antd";
 import { getUser, updateUser } from "../../../services/apiHandle";
 import { debounce, uniqBy } from "lodash";
-import { Typography, Modal, notification } from "antd";
+import { Typography, Modal, notification, Select, Popconfirm } from "antd";
 import { LoadingOutlined } from "@ant-design/icons";
 import { CloseCircleOutlined, CheckCircleOutlined } from "@ant-design/icons";
 import { useSelector } from "react-redux";
@@ -13,16 +13,23 @@ const ManageUser = () => {
 
   const [data, setData] = useState([]);
   const [usernameFilters, setUsernameFilters] = useState([]); // Lưu filters động
+
   const [openModal, setOpenModal] = useState(false);
   const [openConfirmModal, setOpenConfirmModal] = useState(false);
+  const [openModalChangeRole, setOpenModalChangeRole] = useState(false);
+
+  const [tableLoading, setTableLoading] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [loadingForChangeRole, setLoadingForChangeRole] = useState(false);
   const [passwordStatus, setPasswordStatus] = useState(null);
   const [loadingForAction, setLoadingForAction] = useState(false);
   const [actionType, setActionType] = useState(null);
   const [userName, setUserName] = useState(null);
   const [id, setId] = useState(null);
-
+  const [idForChangeRole, setIdForChangeRole] = useState(null);
+  const [roleForChange, setRoleForChange] = useState(null);
   const [password, setPassword] = useState(null);
+  const [valueForSelect, setValueForSelect] = useState("Member");
 
   const fetchGetUser = async () => {
     try {
@@ -34,6 +41,7 @@ const ManageUser = () => {
           placement: "topRight",
           duration: 1.5,
         });
+        setTableLoading(false);
         return;
       }
       return result.data;
@@ -44,11 +52,14 @@ const ManageUser = () => {
         placement: "topRight",
         duration: 1.5,
       });
+      setTableLoading(false);
     }
   };
   const fetchData = async () => {
     try {
+      setTableLoading(true);
       const result = await fetchGetUser();
+      setTableLoading(false);
       transformData(result);
     } catch (err) {
       notification.error({
@@ -190,6 +201,52 @@ const ManageUser = () => {
       setLoadingForAction(false);
     }
   };
+  const handleFetchChangeRole = async () => {
+    try {
+      if (valueForSelect === roleForChange) {
+        notification.error({
+          message: "Cannot change to the same role !",
+          placement: "topLeft",
+          duration: 1.5,
+        });
+        return;
+      }
+      setLoadingForChangeRole(true);
+      const user = await fetchGetUser();
+      const filteredUser = user.find((item) => item._id === idForChangeRole);
+      const newUser = { ...filteredUser, role: valueForSelect };
+      const result = await updateUser(idForChangeRole, newUser);
+      if (result.success === false) {
+        notification.error({
+          message: "Error when fetching",
+          description: result.message,
+          placement: "topRight",
+          duration: 1.5,
+        });
+        setLoadingForChangeRole(false);
+        return;
+      }
+      setLoadingForChangeRole(false);
+
+      notification.success({
+        message: "Change role successfully",
+        placement: "topRight",
+        duration: 1.5,
+      });
+      setLoadingForChangeRole(false);
+      setOpenModalChangeRole(false);
+      fetchData();
+    } catch (err) {
+      notification.error({
+        message: err.message,
+        description: "Please try it again later !",
+        placement: "topRight",
+        duration: 1.5,
+      });
+      setLoadingForChangeRole(false);
+    }
+  };
+
   const handleAccountAction = async () => {
     setLoadingForAction(true);
     if (actionType === "reset") {
@@ -247,6 +304,24 @@ const ManageUser = () => {
   const handleSubmit = (e) => {
     if (e.key === "Enter") {
       fetchCheckAdmin();
+    }
+  };
+  const handleChangeRole = (id, role) => {
+    const status = localStorage.getItem("AdminUnlocked");
+    if (status === "true") {
+      if (user.user_id === id) {
+        notification.error({
+          message: "Cannot change role of your account !",
+          placement: "topLeft",
+          duration: 1.5,
+        });
+        return;
+      }
+      setRoleForChange(role);
+      setIdForChangeRole(id);
+      setOpenModalChangeRole(true);
+    } else {
+      setOpenModal(true);
     }
   };
   const columns = [
@@ -309,8 +384,14 @@ const ManageUser = () => {
       title: "Role",
       key: "role",
       dataIndex: "role",
-      render: (role) => (
-        <Tag color={role === "Admin" ? "red" : "green"}>{role}</Tag>
+      render: (_, record) => (
+        <Tag
+          className="cursor-pointer"
+          onClick={() => handleChangeRole(record.id, record.role)}
+          color={record.role === "Admin" ? "red" : "green"}
+        >
+          {record.role}
+        </Tag>
       ),
     },
     {
@@ -408,17 +489,22 @@ const ManageUser = () => {
 
         {/* 🔹 Hiển thị bảng dữ liệu */}
         <Table
+          className="  roboto-slab-base"
           columns={columns}
-          dataSource={data.filter((user) =>
-            user.username.toLowerCase().includes(searchText)
-          )}
-          className="roboto-slab-base"
+          dataSource={
+            tableLoading
+              ? []
+              : data.filter((user) =>
+                  user.username.toLowerCase().includes(searchText)
+                )
+          }
           scroll={{ x: "max-content" }} // 🔹 Hỗ trợ kéo ngang nếu nhiều cột
           pagination={{
             pageSize: 8,
             showTotal: (total, range) =>
               `${range[0]}-${range[1]} of ${total} users`,
           }}
+          loading={tableLoading}
         />
       </div>
       <Modal
@@ -489,6 +575,48 @@ const ManageUser = () => {
               actionType === "reset" ? "password" : "account"
             } ?`}</h1>
           </div>
+        </div>
+      </Modal>
+      <Modal
+        open={openModalChangeRole}
+        onCancel={() => setOpenModalChangeRole(false)}
+        footer={null}
+      >
+        <Select
+          value={valueForSelect}
+          onChange={(value) => setValueForSelect(value)}
+          size="large"
+          style={{ width: "30%" }}
+          options={[
+            { value: "Admin", label: <span>Admin</span> },
+            { value: "Member", label: <span>Member</span> },
+          ]}
+        />
+        <div className="mt-4 flex justify-end gap-4">
+          <Button
+            color="volcano"
+            variant="outlined"
+            onClick={() => setOpenModalChangeRole(false)}
+            disabled={loadingForChangeRole}
+          >
+            Cancel
+          </Button>
+          <Popconfirm
+            title="Are you sure you want to change the role?"
+            placement="bottomRight"
+            onConfirm={() => handleFetchChangeRole()} // Gọi hàm xử lý
+            onCancel={() => console.log("User cancelled")}
+            okText="Yes"
+            cancelText="No"
+          >
+            <Button
+              type="primary"
+              // loading={loadingForChangeRole}
+              disabled={loadingForChangeRole}
+            >
+              Confirm
+            </Button>
+          </Popconfirm>
         </div>
       </Modal>
     </>
